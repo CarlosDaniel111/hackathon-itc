fef7eff4c5aee57648931172f23491e746495ee6
CarlosDaniel111 <beltranmed.carlos03@gmail.com>
2025-05-22 23:08:18 -0700

    Init project

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+
+const app = express();
+
+// CORS
+app.use(cors());
+
+// Directorio Publico
+app.use(express.static("public"));
+
+// Lectura y parseo del body
+app.use(express.json());
+
+//Rutas
+
+// Abrir servidor
+app.listen(process.env.PORT, () => {
+  console.log(`Servidor corriendo en puerto ${process.env.PORT || 4000}`);
+})
